c6870728d27f8eaf091c4280f1eea6d5cc395564
linsalrob <raedwards@gmail.com>
2025-02-25 15:07:16 +1030

    exposing new methods

--- cf_analysis_lib/__init__.py
+++ cf_analysis_lib/__init__.py
@@ -6,9 +6,9 @@ from .read_data import read_taxonomy, read_metadata, corrections, read_subsystem
 from .metadata_data import metadata_definitions
 from .clean_data import compatible_columns, categories_to_numeric, remove_highly_correlated_data
 from .pathogens import BacterialPathogens
-from .random_forests import random_forest_classifier, random_forest_regression, gb_classifier, gb_regressor
+from .random_forests import random_forest_classifier, random_forest_regression, gb_classifier, gb_regressor, gb_classifier_model, gb_regressor_model
 from .plot_figures import plot_pca, plot_feature_importance, plot_feature_abundance, plot_top_features
-from .plot_figures import plot_abundance_stripplot, create_custom_labels
+from .plot_figures import plot_abundance_stripplot, create_custom_labels, plot_one_feature
 from .export_to_docx import pd2docx
 
 __all__ = ['read_taxonomy', 'read_metadata', 'corrections', 'read_subsystems',
@@ -16,6 +16,8 @@ __all__ = ['read_taxonomy', 'read_metadata', 'corrections', 'read_subsystems',
            'metadata_definitions', 'read_the_data',
            'BacterialPathogens',
            'random_forest_classifier', 'random_forest_regression', 'gb_classifier', 'gb_regressor', 'plot_top_features',
+           'gb_classifier_model', 'gb_regressor_model',
            'plot_pca', 'plot_feature_importance', 'plot_feature_abundance', 'plot_top_features', 'plot_abundance_stripplot',
+           'plot_one_feature',
            'pd2docx', 'create_custom_labels'
         ]
